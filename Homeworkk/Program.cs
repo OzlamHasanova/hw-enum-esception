6aafe302c992201caf97444e4d497c9d253de0ea
OzlamHasanova <7qk9bme@code.edu.az>
2022-11-25 02:21:17 +0400

    enum,excaption

---
+++ Homeworkk/Program.cs
@@ -0,0 +1,33 @@
+﻿using Homeworkk.Utils;
+
+namespace Homeworkk
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Exchange exchange = new Exchange();
+            double money = double.Parse(Console.ReadLine());
+            Console.WriteLine(exchange.Currencymethod(Currencyenum.USD,money));
+        }
+    }
+    public class Exchange
+    {
+        public double Currencymethod(Currencyenum currency, double azn)
+        {
+            
+            double result = currency switch
+            {
+                Currencyenum.USD => 0.59 * azn,
+                Currencyenum.EUR => 0.53 * azn,
+                Currencyenum.TRY => 8.73 * azn,
+                Currencyenum.RUB => 52.21 * azn,
+                _ => 0,
+
+            };
+            return result;
+            
+
+        }
+    }
+}
---
+++ task2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class Student
+    {
+        public static int Id;
+        public static int id;
+        public string Name;
+        public string Surname;
+        public string GroupNo;
+
+        public Student(string name, string surname, string groupNo)
+        {
+            
+            Name = name;
+            Surname = surname;
+            GroupNo = groupNo;
+            id++;
+            id = Id;
+
+        }
+        
+        public string StudentInfo() => $"Id{Id}-Name{Name}-Surname{Surname}-GroupNo{GroupNo}";
+    }
+}
